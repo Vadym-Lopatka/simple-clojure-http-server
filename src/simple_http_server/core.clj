41cab748c22dbf6e7e6e86571807a9baa3d0b2a4
vlopatka <vadim.lopatka@gmail.com>
2023-05-09 19:34:17 +0300

    added users enpoints

--- src/simple_http_server/core.clj
+++ src/simple_http_server/core.clj
@@ -1,5 +1,9 @@
 (ns simple-http-server.core
-  (:require [ring.adapter.jetty :refer [run-jetty]])
+  (:require 
+   [ring.adapter.jetty :refer [run-jetty]]
+   [reitit.ring :as ring]
+   [muuntaja.core :as m]
+   [reitit.ring.middleware.muuntaja :as muuntaja])
   (:gen-class))
 
 "* '/' Return string on base URL"
@@ -7,13 +11,45 @@
 "* '/users/:id' Return user by id"
 "* '/users' POST a user"
 
-(defn handler [request]
+(def users (atom {}))
+
+
+(defn string-handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+   :body "Hello from the string-handler"})
+
+(defn create-user [{user :body-params}]
+  (let [id (str (java.util.UUID/randomUUID))
+        users (->> (assoc user :id id)
+                   (swap! users assoc id))]
+    {:status 200 
+     :body users}))
+
+(defn get-users [_]
+  {:status 200
+   :body @users})
+
+(defn get-user-by-id [{{:keys [id]} :path-params}]
+  {:status 200
+   :body (get @users id)})
+
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    ["/"
+     ["users/:id" {:get get-user-by-id}]
+     ["users" {:get get-users
+               :post create-user}]
+     ["" string-handler]]
+    {:data {:muuntaja m/instance 
+            :middleware [muuntaja/format-middleware]}})))
+
+
 
 (defn start []
-  (run-jetty handler {:port 3000 
+  (run-jetty #'app {:port 3000 
                       :join? false}))
 
 (defn -main
